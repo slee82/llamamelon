3d2ec246655f2d607f2689c1f5336e644f22504c
ciphwn <ciphwn@80533b2c-c34e-11de-bf1b-b7e8a57ece6a>
2009-11-27 05:32:41 +0000

    __Integrating changes from the cipta_1 branch__
    1. Can print a hello world program to stdout (redirect stdout to a file to get the java program)
    2. Uses packages
    3. Uses an Ant build file

---
+++ src/codegen/Program.java
@@ -0,0 +1,30 @@
+package codegen;
+
+import java.util.LinkedList;
+
+public class Program extends ParseTreeNode {
+
+    public Program(LinkedList stmts, String outname) {
+        this.outname = outname;
+        this.statements = stmts;
+    }
+
+    /**
+     * for now, gen() prints to stdout
+     */
+    public void gen() {
+        System.out.println("public class " + outname + " {");
+        System.out.println("\tpublic static void main (String args[]) "+
+                "throws Exception {");
+        
+        for (Object each : statements) {
+            Stmt cur = (Stmt) each;
+            cur.gen();
+        }
+        
+        System.out.println("\t}\n}");
+    }
+    
+    private String outname;
+    private LinkedList statements;
+}
