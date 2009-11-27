8aec0de99874027ac694384d66d68fa627ad01cb
ciphwn <ciphwn@80533b2c-c34e-11de-bf1b-b7e8a57ece6a>
2009-11-27 05:32:41 +0000

    __Integrating changes from the cipta_1 branch__
    1. Can print a hello world program to stdout (redirect stdout to a file to get the java program)
    2. Uses packages
    3. Uses an Ant build file

---
+++ trunk/src/lexer/Keyword.java
@@ -0,0 +1,17 @@
+package lexer;
+
+public class Keyword extends Token {
+    
+    public final String lexeme;
+
+    public Keyword(Tag t, String lex) {
+        super(t);
+        this.lexeme = lex;
+    }
+    
+    /*
+     * Premade keywords
+     */
+    public static final Keyword
+        print = new Keyword(Tag.PRINT, "print");
+}
---
+++ trunk/src/lexer/StringConst.java
@@ -0,0 +1,16 @@
+package lexer;
+
+public class StringConst extends Token {
+    
+    public String val;
+
+    public StringConst(String s) {
+        super(Tag.STRINGCONST);
+        this.val = s;
+    }
+    
+    public String toString() {
+        return ("(" + super.toString() + " " + val + ")");
+    }
+
+}
