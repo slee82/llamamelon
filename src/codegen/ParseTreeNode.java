8f4cd8e61e8140068008993b9475ec02872503f5
daniellasry <daniellasry@80533b2c-c34e-11de-bf1b-b7e8a57ece6a>
2009-12-04 07:24:16 +0000

    Merged my branch into trunk

--- src/codegen/ParseTreeNode.java
+++ src/codegen/ParseTreeNode.java
@@ -1,3 +1,9 @@
+/*
+ * COMS W4119 PROGRAMMING LANGUAGES AND TRANSLATORS FALL 2009
+ * Team llamamelon - BALL language
+ * ParseTreeNode.java
+ */
+
 package codegen;
 
 public abstract class ParseTreeNode {
--- src/codegen/PrintStmt.java
+++ src/codegen/PrintStmt.java
@@ -1,3 +1,9 @@
+/*
+ * COMS W4119 PROGRAMMING LANGUAGES AND TRANSLATORS FALL 2009
+ * Team llamamelon - BALL language
+ * PrintStmt.java - Code Generator for print statements
+ */
+
 package codegen;
 
 public class PrintStmt extends Stmt {
