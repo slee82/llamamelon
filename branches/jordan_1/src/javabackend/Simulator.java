a6331ff4df67cccd8d9de778ab82f1aa3fca5181
jordan.schau <jordan.schau@80533b2c-c34e-11de-bf1b-b7e8a57ece6a>
2009-12-09 22:26:57 +0000

    Making new branch!  pureneeds

---
+++ branches/jordan_1/src/javabackend/Simulator.java
@@ -0,0 +1,17 @@
+package javabackend;
+
+/*
+ * COMS W4119 PROGRAMMING LANGUAGES AND TRANSLATORS FALL 2009
+ * Team llamamelon - BALL language
+ * Simulator.java - the sim() wrapper function
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class Simulator {
+
+    public static TeamObj sim(TeamObj team1, TeamObj team2, float number){
+    	return null;
+    }
+}
