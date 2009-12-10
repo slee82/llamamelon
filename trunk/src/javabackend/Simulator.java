1b14034923332d80b0e747f4002f910053d25c00
daniellasry <daniellasry@80533b2c-c34e-11de-bf1b-b7e8a57ece6a>
2009-12-10 01:13:06 +0000

    Tested sim() a little more, going to brach off soon and implement activation statements.

--- trunk/src/javabackend/Simulator.java
+++ trunk/src/javabackend/Simulator.java
@@ -10,7 +10,8 @@ import java.util.*;
 import java.io.*;
 
 public class Simulator {
-    //Main function just for testing
+	
+    /*Main function just for testing. (TODO: Delete)*/
     public static void main(String args[]){
 	theSimFunction = new SimFunction(){
 			public TeamObj doSim(TeamObj team1, TeamObj team2){
@@ -18,21 +19,29 @@ public class Simulator {
 			}
 		};
 
-	System.out.println(sim(Loader.load("dodgers.team"), Loader.load("astros.team"), 1));
+	System.out.println(sim(Loader.load("dodgers.team"), Loader.load("astros.team"), 6));
     }
 
+    /* sim() function
+     * Takes two teams and a number, returns the winner.*/
     public static TeamObj sim(TeamObj team1, TeamObj team2, float number){
+	//Keep track of how many times each team won
 	int team1Wins=0;
 	int team2Wins=0;
-
+	
+	//Ge the number of times to play
 	int timesToPlay = (int) number;
 
 	int plays=0;
+	/* Play a certain number of times, if the winner cannot be determined, play more*/
 	while(team1Wins==team2Wins || plays < timesToPlay){
-		if(theSimFunction.doSim(team1, team2) == team1)
+		//Determine the winner
+		TeamObj winner = theSimFunction.doSim(team1, team2);
+		if(winner == team1)
 		    team1Wins++;
-		else if(theSimFunction.doSim(team1, team2) == team2)
+		else if(winner == team2)
 		    team2Wins++;
+
 		plays++;
 		if (plays > timesToPlay+20){
 		    System.out.println("Could not determine a winner after 20 tries, returning team1.");
