100ab2626106d7e9fe7cf77c97dcbde15aedbe38
daniellasry <daniellasry@80533b2c-c34e-11de-bf1b-b7e8a57ece6a>
2009-12-10 00:45:23 +0000

    sim() working

--- trunk/src/javabackend/Simulator.java
+++ trunk/src/javabackend/Simulator.java
@@ -10,6 +10,7 @@ import java.util.*;
 import java.io.*;
 
 public class Simulator {
+    //Main function just for testing
     public static void main(String args[]){
 	theSimFunction = new SimFunction(){
 			public TeamObj doSim(TeamObj team1, TeamObj team2){
@@ -21,7 +22,29 @@ public class Simulator {
     }
 
     public static TeamObj sim(TeamObj team1, TeamObj team2, float number){
-    	return theSimFunction.doSim(team1, team2);
+	int team1Wins=0;
+	int team2Wins=0;
+
+	int timesToPlay = (int) number;
+
+	int plays=0;
+	while(team1Wins==team2Wins || plays < timesToPlay){
+		if(theSimFunction.doSim(team1, team2) == team1)
+		    team1Wins++;
+		else if(theSimFunction.doSim(team1, team2) == team2)
+		    team2Wins++;
+		plays++;
+		if (plays > timesToPlay+20){
+		    System.out.println("Could not determine a winner after 20 tries, returning team1.");
+		    return team1;
+		}
+		
+	}
+	
+	if(team1Wins>team2Wins)
+		return team1;
+	else
+		return team2;
     }
 
     public static SimFunction theSimFunction;
