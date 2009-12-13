97210ca629bef348a2edef3cd81dcd42ac9fbfa4
ciphwn <ciphwn@80533b2c-c34e-11de-bf1b-b7e8a57ece6a>
2009-12-13 00:56:50 +0000

    '+' operator overloading (string+string, list+list, num+num)

--- branches/cipta_6/src/javabackend/BallList.java
+++ branches/cipta_6/src/javabackend/BallList.java
@@ -8,6 +8,7 @@ public class BallList extends ArrayList {
     @SuppressWarnings("unchecked")
     public BallList(Object[] create) {
         super(Arrays.asList(create));
+        orig = create;
     }
     
     public String toString() {
@@ -28,7 +29,23 @@ public class BallList extends ArrayList {
         begin += " ]";
         return begin;
     }
+    
+    @SuppressWarnings("unchecked")
+    public BallList append(BallList right) {
+        BallList result = new BallList(Arrays.copyOf(this.orig, this.orig.length));
+        if (this.size() > 0 && right.size() > 0) {
+            // align types
+            Object ol = this.get(0);
+            Object or = right.get(0);
+            if (!(ol.getClass().equals(or.getClass()))) {
+                throw new RuntimeException("run: type of lists do not match.");
+            }
+        }
+        result.addAll(right);
+        return result;
+    }
 
+    private Object[] orig;
     private static final long serialVersionUID = 1L;
 
 }
