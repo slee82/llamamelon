873798d72b53630714c48eb958ccc677f53efd9f
slee82 <slee82@users.noreply.github.com>
2009-12-19 17:29:03 +0000

    added fixFloat to Tools.java

--- src/javabackend/Tools.java
+++ src/javabackend/Tools.java
@@ -15,6 +15,22 @@ public class Tools {
         return myRand.nextInt(max-min) + min;
     }
     
+    public static String fixFloat(float input) {
+    	String str ="";
+    	if (input > 1.0 && input != Math.floor(input)) {
+    		str = String.format("%.2f",input);
+    	}
+    	
+    	else if (input == Math.floor(input))
+    	{ str = String.format("%.0f",input); }
+    	
+    	else if (input < 1.0){ 
+    		str = String.format("%.3f",input); 
+    	}
+    	return str;
+    }
+
+    
     private static final Random myRand = new Random(new java.util.Date().getTime());
 
 }
