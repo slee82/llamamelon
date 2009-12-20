b87c1fade2e6c2f868deac6f9a576d1aca18dfb5
jordan.schau <jordan.schau@80533b2c-c34e-11de-bf1b-b7e8a57ece6a>
2009-12-20 12:23:07 +0000

    added headers

--- src/codegen/FuncDef.java
+++ src/codegen/FuncDef.java
@@ -1,3 +1,9 @@
+/*
+ * COMS W4115 PROGRAMMING LANGUAGES AND TRANSLATORS FALL 2009
+ * Team llamamelon - BALL language
+ * Authors: Cipta Herwana, Daniel Lasry, Sam Lee, Nathan Miller, Jordan Schau 
+ * FuncDef.java - Adds function defs to symbol table
+ */
 package codegen;
 
 import java.util.ArrayList;
--- src/codegen/InsertionPoint.java
+++ src/codegen/InsertionPoint.java
@@ -1,3 +1,9 @@
+/*
+ * COMS W4115 PROGRAMMING LANGUAGES AND TRANSLATORS FALL 2009
+ * Team llamamelon - BALL language
+ * Authors: Cipta Herwana, Daniel Lasry, Sam Lee, Nathan Miller, Jordan Schau 
+ * InsertionPoint.java - Defines location for insertion statements
+ */
 package codegen;
 
 /**
