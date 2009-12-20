78e55ba86ca790b7bdd11ffee95a6f381f9e03fa
jordan.schau <jordan.schau@80533b2c-c34e-11de-bf1b-b7e8a57ece6a>
2009-12-20 12:23:07 +0000

    added headers

--- trunk/src/codegen/IfStmt.java
+++ trunk/src/codegen/IfStmt.java
@@ -1,6 +1,7 @@
 /*
  * COMS W4115 PROGRAMMING LANGUAGES AND TRANSLATORS FALL 2009
  * Team llamamelon - BALL language
+ * Authors: Cipta Herwana, Daniel Lasry, Sam Lee, Nathan Miller, Jordan Schau 
  * IfStmt.java - nodes representing 'if' statements
  */
 
