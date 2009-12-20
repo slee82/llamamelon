eab4b677284108943c1ca01f1409c862374c19b4
jordan.schau <jordan.schau@80533b2c-c34e-11de-bf1b-b7e8a57ece6a>
2009-12-20 12:22:52 +0000

    added headers

--- trunk/src/codegen/LogicalExpr.java
+++ trunk/src/codegen/LogicalExpr.java
@@ -1,3 +1,9 @@
+/*
+ * COMS W4115 PROGRAMMING LANGUAGES AND TRANSLATORS FALL 2009
+ * Team llamamelon - BALL language
+ * Authors: Cipta Herwana, Daniel Lasry, Sam Lee, Nathan Miller, Jordan Schau 
+ * LogicalExpr.java - Generates code for logical Expressions
+ */
 package codegen;
 
 import compiler.SymbolTable;
